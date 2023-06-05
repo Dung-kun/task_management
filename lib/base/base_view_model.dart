1eec1fc01c3065c4ca1201d5c56e3d286f0c93c9
thanhdancodoc <thanhdancodoc@gmail.com>
2023-06-05 23:05:17 +0700

    add firebase messing service

--- lib/base/base_view_model.dart
+++ lib/base/base_view_model.dart
@@ -2,21 +2,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rxdart/rxdart.dart';
-
+import 'package:to_do_list/services/firestore_messing_service.dart';
 export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'package:rxdart/rxdart.dart';
 
-import '/providers/auth_provider.dart';
 
 abstract class BaseViewModel {
   BehaviorSubject<bool> bsLoading = new BehaviorSubject.seeded(false);
   BehaviorSubject<bool> bsRunning = new BehaviorSubject.seeded(false);
 
   final AutoDisposeProviderReference ref;
-
+  late final FirestoreMessagingService firestoreMessagingService;
   User? user;
 
-  BaseViewModel(this.ref) {}
+  BaseViewModel(this.ref) {
+  }
 
   @mustCallSuper
   void dispose() {
