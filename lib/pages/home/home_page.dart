12d3c558e0ca13ab84c2c863d09cdc56fad9f35c
thanhdancodoc <thanhdancodoc@gmail.com>
2023-06-06 23:51:17 +0700

    fix error

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:to_do_list/models/project_model.dart';
+
+import '/base/base_state.dart';
+import '/constants/constants.dart';
+import '/util/extension/extension.dart';
+import 'home_provider.dart';
+import 'home_vm.dart';
+import 'tab/my_task/my_task_tab.dart';
+import 'tab/my_note/my_note_tab.dart';
+import 'widgets/add_new_button.dart';
+
+class HomePage extends StatefulWidget {
+  final ScopedReader watch;
+
+  static Widget instance() {
+    return Consumer(builder: (context, watch, _) {
+      return HomePage._(watch);
+    });
+  }
+
+  const HomePage._(this.watch);
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends BaseState<HomePage, HomeViewModel> {
+  int currentTab = 0;
+  PageController tabController = PageController();
+  ProjectModel? projectMode;
+
+  late AndroidNotificationChannel channel;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  void logOutClick() {
+    getVm().logOut();
+  }
+
+  List<Widget> tabWidget = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    //notification
+    getVm().initMessingToken();
+    //tab widget
+    tabWidget = [
+      MyTaskTab.instance(mode: projectMode, closeProjectMode: closeProjectMode),
+      MyNoteTab.instance(
+        mode: projectMode,
+      ),
+    ];
+  }
+
+  void setProjectMode(ProjectModel value) async {
+    setState(() {
+      projectMode = value;
+      tabWidget[0] = MyTaskTab.instance(
+          mode: projectMode, closeProjectMode: closeProjectMode);
+    });
+    await tabController.animateToPage(
+      0,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void closeProjectMode() async {
+    setState(() {
+      projectMode = null;
+      tabWidget[0] = MyTaskTab.instance(
+          mode: projectMode, closeProjectMode: closeProjectMode);
+    });
+    await tabController.animateToPage(
+      0,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void tabClick(int index) {
+    if (index > 1) {
+      setState(() {
+        currentTab = index - 1;
+      });
+    } else {
+      setState(() {
+        currentTab = index;
+      });
+    }
+    tabController.animateToPage(
+      currentTab,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void goTab(int index) {
+    setState(() {
+      currentTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: buildBody(),
+      floatingActionButton: AddNewButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: buildBottomNavigationBar(
+        currentIndex: currentTab,
+        press: tabClick,
+      ),
+    );
+  }
+
+  Container buildBody() {
+    return Container(
+      width: double.infinity,
+      height: screenHeight,
+      child: PageView.builder(
+        itemCount: 4,
+        onPageChanged: (index) => goTab(index),
+        itemBuilder: (context, index) => tabWidget[index],
+        controller: tabController,
+      ),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar({
+    required int currentIndex,
+    required Function press,
+  }) {
+    return BottomNavigationBar(
+      selectedIconTheme: IconThemeData(color: Colors.white),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      backgroundColor: Color(0xFF292E4E),
+      items: [
+        buildBottomNavigationBarItem(
+          title: StringTranslateExtension(AppStrings.myTask).tr(),
+          icon: AppImages.myTaskIcon,
+          index: 0,
+        ),
+        buildBottomNavigationBarItem(
+          title: StringTranslateExtension(AppStrings.project).tr(),
+          icon: AppImages.menuIcon,
+          index: 1,
+        ),
+        BottomNavigationBarItem(icon: Container(), label: ""),
+        buildBottomNavigationBarItem(
+          title: StringTranslateExtension(AppStrings.myNote).tr(),
+          icon: AppImages.quickIcon,
+          index: 2,
+        ),
+        buildBottomNavigationBarItem(
+          title: StringTranslateExtension(AppStrings.profiles).tr(),
+          icon: AppImages.profileIcon,
+          index: 3,
+        ),
+      ],
+      onTap: (index) => press(index),
+    );
+  }
+
+  BottomNavigationBarItem buildBottomNavigationBarItem({
+    required String title,
+    required String icon,
+    required int index,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(
+          top: 4,
+        ),
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              icon,
+              color: Colors.white.withOpacity(currentTab == index ? 1 : .5),
+              width: 24.w,
+              height: 24.w,
+            ),
+            SizedBox(height: 4),
+            title
+                .plain()
+                .fSize(12)
+                .color(Colors.white.withOpacity(currentTab == index ? 1 : .5))
+                .b()
+          ],
+        ),
+      ),
+      label: title,
+    );
+  }
+
+  @override
+  HomeViewModel getVm() => widget.watch(viewModelProvider).state;
+}
